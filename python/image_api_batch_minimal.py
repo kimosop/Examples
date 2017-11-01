3931347d47aa025afec5546af97af644cf462a19
jens25 <jens@peat.ai>
2017-11-01 14:21:36 +0100

    Fixed json metadata loading.

--- python/image_api_batch_minimal.py
+++ python/image_api_batch_minimal.py
@@ -35,7 +35,7 @@ def load_json(json_file, folder):
     json_data = {}
     if json_file:
         with open(os.path.join(folder, json_file[0])) as data_file:
-            json_data = json.load(data_file)
+            json_data = json.load(data_file, parse_float=str, parse_int=str)
     return json_data
 
 
@@ -47,22 +47,19 @@ def batch_processing(directory):
     '''
 
     # get a list of all the subfolders
-    folderlist = [x[0] for x in os.walk(directory)]
+    folder_list = [x[0] for x in os.walk(directory)]
     # iterate over all folder in a given directory
-    for folder in folderlist:
-        filelist = [i for i in os.listdir(folder) if i.endswith('.jpg') or i.endswith('.png')]
+    for folder in folder_list:
+        file_list = [i for i in os.listdir(folder) if i.endswith('.jpg') or i.endswith('.png')]
 
         # Find the json metadata file in the folder
         json_file = [i for i in os.listdir(folder) if i == JSON_META_FILENAME]
-
         json_meta_data = load_json(json_file, folder)
-
         # Merge the header with the metadata json.
         new_header = json_meta_data.copy()
         new_header.update(HEADER)
-
         # iterate over all files in a given subfolder
-        for f in filelist:
+        for f in file_list:
             filepath = os.path.join(folder, f)
 
             # Send request
